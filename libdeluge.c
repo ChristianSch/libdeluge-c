5b6fb9d7bc6f628ad30a748da41775ea39e3a1d5
Christian Schulze <christian.schulze@mni.thm.de>
2015-12-28 00:38:32 +0100

    changed from sockets to OpenSSL BIO. added trusted store, cert validation  (fails ¯\_(ツ)_/¯) and moar comments

--- libdeluge.c
+++ libdeluge.c
@@ -9,6 +9,7 @@
 #include <resolv.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <openssl/bio.h>
 #include "zlib.h"
 
 struct WriteMsg {
@@ -39,7 +40,7 @@ struct WriteMsg *encode_str(char* str)
     uLong compSize = compressBound(ucompSize);
     uLong targetSize = compSize + 5;
 
-    printf("%lu/%lu/%lu len\n", ucompSize, compSize, targetSize);
+    /* printf("%lu/%lu/%lu len\n", ucompSize, compSize, targetSize); */
 
     if (ret == NULL)
         error("Ran out of memory.");
@@ -60,11 +61,13 @@ struct WriteMsg *encode_str(char* str)
     out[4] = (len >> 24);
 
     /* debug */
+    /*
     printf("out:\n");
     printf("[ ");
     for (i = 0; i < compSize; i++)
         printf("%d, ", out[i]);
     printf(" ]\n");
+    */
 
     /* header is 5 bytes, no NUL */
     ret->str = malloc(sizeof(char) * compSize + 5);
@@ -94,12 +97,9 @@ char *decode_str(char* str, int len)
 
 int main(int argc, char** argv)
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+    BIO * bio;
+    int n;
     char buffer[5000];
-
-    const SSL_METHOD *method;
     SSL_CTX *ctx;
     SSL *ssl;
 
@@ -110,77 +110,78 @@ int main(int argc, char** argv)
     OpenSSL_add_all_algorithms();
     SSL_library_init();
     SSL_load_error_strings();
+    ERR_load_BIO_strings();
 
-    method = SSLv23_client_method();
-    assert(method != NULL);
-    ctx = SSL_CTX_new(method);
+    ctx = SSL_CTX_new(SSLv23_client_method());
     assert(ctx != NULL);
     SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
 
+    /* NOTE: here we *should* look all around the OS for the file instead of assuming
+     * that OpenSSL was installed via homebrew. And this even validates only for the time
+     * being. If someone is super clever and changes the dir, we're f*cked.
+     * Yeah, it's really that … weird.
+     *
+     * However, you could also just use the directory. I'd hint to the doc, but there is
+     * none. For real. This could come in handy though:
+     *  http://openssl.6102.n7.nabble.com/SSL-CTX-set-default-verify-paths-and-Windows-td25299.html
+     *
+     */
+    if (!SSL_CTX_load_verify_locations(ctx, "/usr/local/etc/openssl/cert.pem", NULL))
+        error("Could not load trusted store");
+
     /* encode_str("[[1,'info',[],{}]] "); */
-
-    portno = 58846;
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0)
-        error("No socket opened.");
-
-    server = gethostbyname("127.0.0.1");
-
-    if (server == NULL)
-        fprintf(stderr, "No host.");
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-    serv_addr.sin_addr.s_addr = *(long *)(server->h_addr);
-
-
-    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
 
     ssl = SSL_new(ctx);
     assert(ssl != NULL);
-    SSL_set_fd(ssl, sockfd);
-    SSL_connect(ssl);
+
+    bio = BIO_new_ssl_connect(ctx);
+
+    if (bio == NULL)
+        error("Connection init error");
+
+    BIO_get_ssl(bio, &ssl);
+    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+
+    BIO_set_conn_hostname(bio, "localhost:58846");
+
+    if (BIO_do_connect(bio) <= 0)
+        error("Connection could not be established");
+
+    /* Guess what! Deluge's cert does not validate, or something is wrong. I dunno. */
+    /*
+    if (SSL_get_verify_result(ssl) != X509_V_OK)
+        error("SSL cert could not be verified");
+    */
 
     bzero(buffer, 5000);
 
     /* authorize */
-    printf("to write: %lu\n", auth->len);
-    n = SSL_write(ssl, &auth->str, auth->len);
-    //n = write(sockfd, &auth->str, auth->len); /* <- this works … kinda */
-    if (n < 0)
-        error("ERROR writing to socket.");
-    printf("wrote %d bytes\n", n);
+    if ((n = BIO_write(bio, &auth->str, auth->len)) < 0)
+        error("ERROR writing to socket");
 
-    n = SSL_read(ssl, buffer, 255);
-    printf("got %d bytes\n", n);
+    assert(n == auth->len);
 
-    if (n < 0)
+    if ((n = BIO_read(bio, buffer, 255)) < 0)
         error("ERROR reading");
 
-    printf("%d: `%s`\n", n, buffer);
+    printf("%dB `%s`\n", n, buffer);
     printf("%s\n", decode_str(buffer, n));
 
     /* get torrent list */
     bzero(buffer, 5000);
-    n = SSL_write(ssl, &get_torrents->str, get_torrents->len);
-
-    if (n < 0)
+    if ((n = BIO_write(bio, &get_torrents->str, get_torrents->len)) < 0)
         error("ERROR writing");
 
-    n = SSL_read(ssl, buffer, 4999);
+    assert(n == get_torrents->len);
 
-    if (n < 0)
+    if ((n = BIO_read(bio, buffer, 4999)) < 0)
         error("ERROR reading");
 
     printf("%d: %s\n", n, buffer);
     printf("%s\n", decode_str(buffer, n));
 
+    BIO_free_all(bio);
     SSL_free(ssl);
-    close(sockfd);
     SSL_CTX_free(ctx);
 
     return 0;
