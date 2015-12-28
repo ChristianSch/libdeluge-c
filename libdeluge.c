c93390ceaeeabc3ebb0daccb30bc6369133b6775
Christian Schulze <christian.schulze@mni.thm.de>
2015-12-28 18:34:40 +0100

    improved ssl setup; reusing socket; added debug prints

--- libdeluge.c
+++ libdeluge.c
@@ -48,7 +48,8 @@ struct WriteMsg *encode_str(char* str)
     if ((out = malloc(sizeof(char) * (compSize + 5))) == NULL)
         error("Ran out of memory.");
 
-    compress((Bytef *)out + 5, &compSize, (Bytef *)str, ucompSize);
+    if (compress((Bytef *)out + 5, &compSize, (Bytef *)str, ucompSize) != Z_OK)
+        error("Could not deflate message");
 
     /* the header */
     len = htonl(ucompSize);
@@ -86,15 +87,18 @@ char *decode_str(char* str, int len)
     if ((out = malloc(sizeof(char) * len)) == NULL)
         error("Ran out of memory.");
 
-    uncompress((Bytef *)out, &ucompSize, (Bytef *)str, compSize);
+    if (uncompress((Bytef *)out, &ucompSize, (Bytef *)str, compSize) != Z_OK)
+        error("Could not inflate message");
 
     return out;
 }
 
 int main(int argc, char** argv)
 {
-    BIO * bio;
-    int n;
+    BIO *sbio;
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
     char buffer[5000];
     SSL_CTX *ctx;
     SSL *ssl;
@@ -103,14 +107,12 @@ int main(int argc, char** argv)
     struct WriteMsg *get_torrents = encode_str("[[2,'daemon.get_torrents_status',[],{}]]");
 
     /* init openssl */
-    OpenSSL_add_all_algorithms();
     SSL_library_init();
     SSL_load_error_strings();
-    ERR_load_BIO_strings();
 
+    /* setting up the openssl context */
     ctx = SSL_CTX_new(SSLv23_client_method());
     assert(ctx != NULL);
-    SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
 
     /* NOTE: here we *should* look all around the OS for the file instead of assuming
      * that OpenSSL was installed via homebrew. And this even validates only for the time
@@ -127,21 +129,36 @@ int main(int argc, char** argv)
 
     /* encode_str("[[1,'info',[],{}]] "); */
 
+    /* init socket */
+    portno = 58846;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        error("No socket opened.");
+
+    if ((server = gethostbyname("127.0.0.1")) == NULL)
+        fprintf(stderr, "No host.");
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = *(long *)(server->h_addr);
+
+    if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    /* connect socket to ssl and prepare BIO */
     ssl = SSL_new(ctx);
     assert(ssl != NULL);
 
-    if ((bio = BIO_new_ssl_connect(ctx)) == NULL)
-        error("Connection init error");
-
-    BIO_get_ssl(bio, &ssl);
     SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
 
-    BIO_set_conn_hostname(bio, "localhost:58846");
+    sbio = BIO_new_socket(sockfd, BIO_NOCLOSE);
+    SSL_set_bio(ssl, sbio, sbio);
 
-    if (BIO_do_connect(bio) <= 0)
-        error("Connection could not be established");
+    /* ssl handshake */
+    if (SSL_connect(ssl) < 0)
+        error("SSL handshake failed");
 
-    /* Guess what! Deluge's cert does not validate, or something is wrong. I dunno. */
     /*
     if (SSL_get_verify_result(ssl) != X509_V_OK)
         error("SSL cert could not be verified");
@@ -150,12 +167,16 @@ int main(int argc, char** argv)
     bzero(buffer, 5000);
 
     /* authorize */
-    if ((n = BIO_write(bio, &auth->str, auth->len)) < 0)
+    printf("%lu bytes to write\n", auth->len);
+    if ((n = SSL_write(ssl, &auth->str, auth->len)) < 0)
         error("ERROR writing to socket");
 
     assert(n == auth->len);
 
-    if ((n = BIO_read(bio, buffer, 255)) < 0)
+    /* debug */
+    printf("write done\n");
+
+    if ((n = SSL_read(ssl, buffer, 255)) < 0)
         error("ERROR reading");
 
     printf("%dB `%s`\n", n, buffer);
@@ -163,18 +184,20 @@ int main(int argc, char** argv)
 
     /* get torrent list */
     bzero(buffer, 5000);
-    if ((n = BIO_write(bio, &get_torrents->str, get_torrents->len)) < 0)
+    if ((n = SSL_write(ssl, &get_torrents->str, get_torrents->len)) < 0)
         error("ERROR writing");
 
     assert(n == get_torrents->len);
 
-    if ((n = BIO_read(bio, buffer, 4999)) < 0)
+    if ((n = SSL_read(ssl, buffer, 4999)) < 0)
         error("ERROR reading");
 
     printf("%d: %s\n", n, buffer);
     printf("%s\n", decode_str(buffer, n));
 
-    BIO_free_all(bio);
+    /* clean shutdown */
+    SSL_shutdown(ssl);
+
     SSL_free(ssl);
     SSL_CTX_free(ctx);
 
